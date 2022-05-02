be8e95a14d59af2433b9c2d44d53598f85284914
LaraKra <konlor@rambler.ru>
2022-05-02 16:27:02 +0300

    add test city

--- src/test/java/ru/netology/CardDelivery.java
+++ src/test/java/ru/netology/CardDelivery.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -18,5 +20,6 @@ public class CardDelivery {
     @Test
     public void shouldSendForm() {
         String localD = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $(".input__control[type='text'][placeholder='Город']").setValue("Калининград");
     }
 }
