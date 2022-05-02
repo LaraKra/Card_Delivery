c15f847c27cbc3318f88a167b27688e5adc3af87
LaraKra <konlor@rambler.ru>
2022-05-02 16:21:30 +0300

    add test

--- src/test/java/ru/netology/CardDelivery.java
+++ src/test/java/ru/netology/CardDelivery.java
@@ -1,4 +1,22 @@
 package ru.netology;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Selenide.open;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class CardDelivery {
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    public void shouldSendForm() {
+        String localD = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 }
